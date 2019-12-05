f88c9eb6216ad344bfbc1d17c076113eb2f26b85
LiTao <1287862489@qq.com>
2019-12-06 00:34:32 +0800

    移除未使用文件

--- views/site/percenter.php
+++ views/site/percenter.php
@@ -17,10 +17,10 @@ $this->title = '个人中心';
                 <div class="col-lg-12">
 
                     <div class="loop-conleft">
-                        <h4><i class="fa fa-address-card">登录名：</i><?=$data['username']?></h4>
-                        <h4><i class="fa fa-address-card">昵称：</i><?=$data['nickname']?></h4>
-                        <h4><i class="fa fa-envelope">手机号码：</i><?= Util::format_call($data['phone'])?></h4>
-                        <h4><i class="fa fa-calendar-times-o">注册时间：</i><?=$data['gmt_create']?></h4>
+                        <h4><i class="fa fa-address-card"> 登录名：</i><?=$data['username']?></h4>
+                        <h4><i class="fa fa-address-card"> 昵称：</i><?=$data['nickname']?></h4>
+                        <h4><i class="fa fa-envelope"> 手机号码：</i><?= Util::format_call($data['phone'])?></h4>
+                        <h4><i class="fa fa-calendar-times-o"> 注册时间：</i><?=$data['gmt_create']?></h4>
                     </div>
 
                 </div>
